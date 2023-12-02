1f6b7c2ebb73284fe87582d1016b51c8f4194e67
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2023-12-02 11:01:07 +0000

    songpage created

---
+++ data.jsx
@@ -0,0 +1,28 @@
+var data = [
+    {   
+        img:"https://stat4.bollywoodhungama.in/wp-content/uploads/2016/04/mithoon1.jpg",
+        title: "Daily Mix 1",
+        artist: "Mithon Sharma",
+
+    },
+    {   
+        img:"https://stat4.bollywoodhungama.in/wp-content/uploads/2016/04/mithoon1.jpg",
+        title: "Daily Mix 2",
+        artist: "Mithon Sharma",
+
+    },
+    {   
+        img:"https://stat4.bollywoodhungama.in/wp-content/uploads/2016/04/mithoon1.jpg",
+        title: "Daily Mix 3",
+        artist: "Mithon Sharma",
+
+    },
+    {   
+        img:"https://stat4.bollywoodhungama.in/wp-content/uploads/2016/04/mithoon1.jpg",
+        title: "Daily Mix 4",
+        artist: "Mithon Sharma",
+
+    },
+]
+
+export default data;
---
+++ script.js
@@ -0,0 +1,7 @@
+import data from './data'
+
+function card(){
+    return console.log(data)
+
+}
+card()
